99349a30289468e3f968762aa25fd9348ead6a31
LUIYIGM <151877691+LUIYIGM@users.noreply.github.com>
2023-11-23 22:22:11 -0500

    Cambio boletines nuevamente

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -20,7 +20,7 @@ links.forEach(function(link){
         content.classList.add('animate__animated');
 
         setTimeout(function(){
-            location.href = "/";
+            location.href = "/boletines";
         },500);
 
         // setInterval  bucle ejecutar cada 1 segundo o tiempo asignado

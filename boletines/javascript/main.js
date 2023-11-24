11329f29f1876e9fd8fc5a06a96a59120b288e8f
LUIYIGM <151877691+LUIYIGM@users.noreply.github.com>
2023-11-23 22:11:02 -0500

    Agregamos boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,42 @@
+//Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll('.close');
+
+//Recorrerlos
+
+links.forEach(function(link){
+    //Agregar evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitar las clases de animacion
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases para animar salida
+
+        content.classList.add('animate__fadeOutUp');
+        content.classList.add('animate__animated');
+
+        setTimeout(function(){
+            location.href = "/";
+        },700)
+
+        // setInterval  bucle ejecutar cada 1 segundo o tiempo asignado
+
+
+        return false;
+    });
+
+});
+
+/*
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star-o");
+    icono.classList.add("fa-star");
+});
+
+*/

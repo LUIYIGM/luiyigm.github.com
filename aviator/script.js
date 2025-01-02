dbe3b743ba6fb99caff320fb0cbef13d4a5ea781
LUIYIGM <151877691+LUIYIGM@users.noreply.github.com>
2025-01-02 18:11:12 -0500

    adasd

--- aviator/script.js
+++ aviator/script.js
@@ -17,30 +17,4 @@ function login() {
     }
 }
 
-// stars.js
-const starContainer = document.querySelector(".stars");
-
-function createStars() {
-    for (let i = 0; i < 100; i++) {  // Crear 100 estrellas
-        let star = document.createElement("div");
-        star.classList.add("star");
-        // Posicionamos aleatoriamente las estrellas
-        star.style.top = `${Math.random() * 100}vh`;
-        star.style.left = `${Math.random() * 100}vw`;
-        // Asignamos velocidades y tamaños aleatorios a las estrellas
-        const size = Math.random() * 3 + 1;  // Tamaño de las estrellas
-        const animationDuration = Math.random() * 3 + 3; // Duración de la animación
-
-        star.style.width = `${size}px`;
-        star.style.height = `${size}px`;
-
-        // Aplicamos una animación diferente a cada estrella
-        star.style.animationDuration = `${animationDuration}s`;
-
-        starContainer.appendChild(star);
-    }
-}
-
-// Llamamos a la función para crear las estrellas al cargar la página
-createStars();
 
